d314629601f1b831d3354934c02de3843e96b557
나평근 <skvudrms54@gmail.com>
2020-04-10 03:56:41 +0900

    Edit dataSource 설정, 더 성능좋도록

--- src/main/java/com/poseungcar/webocr/config/RootConfig.java
+++ src/main/java/com/poseungcar/webocr/config/RootConfig.java
@@ -48,22 +48,38 @@ public class RootConfig {
 		ds.setUrl(dbURL); 
 		ds.setUsername(dbUserName); 
 		ds.setPassword(dbPassword); 
-		ds.setInitialSize(2);
-		ds.setMaxActive(10);
-		ds.setMaxIdle(10);
+		//Datasource 성능 튜닝 및 validation 연결 유지
+		
+		
+		ds.setInitialSize(100);                         //initialSize : 풀의 초기 생성되는 사이즈
+		ds.setMaxActive(150);                           //maxActive : 최대 접속가능 커넥션 수 (사실상 가장 중요하다고 생각한다. DB에 동시에 접속 가능 커넥션이 많아야 성능이 좋아진다고 볼 수 있다.)
+		ds.setMaxWait(10000);                           //connection 사용이 많아져서 connection pool이 비었을 때 대기시간 (단위 1/1000초) 기본값은 -1(무한대)이며,  서비스 특성에 맞게 설정하면된다. 일반적으로 기본값을 사용해도 큰 문제는 안될 것 같다.
+		ds.setMaxIdle(30);                               //maxIdle : Idle상태의 최대 갯수                                                                                   
+		ds.setMinIdle(20);                               //minIdle : idle상태의 최소 갯수                                                                                   
+		ds.setTestOnBorrow(true);                        //testOnBorrow : 풀에서 커넥션을 가져올시 커넥션의 유효성 검사                                                                  
+		ds.setTestOnReturn(true);                        //testOnReturn : 풀에 커넥션을 리턴할 때 커넥션의 유효성 검사                                                                  
+		ds.setValidationQuery("select 1");               //validationQuery : validate Query                                                                          
+		ds.setTestWhileIdle(true);                       //testWhileIdle : Idle상태에 커넥션의 유효성 검사                                                                       
+		ds.setTimeBetweenEvictionRunsMillis(130000);     //timeBetweenEvictionRunsMillis : 설정된 주기를 통해 Evict(유효하지 않는 커넥션/정의된 시간이 만료된 커넥션을 풀에서 제거) 쓰레드를 수행             
+		ds.setMinEvictableIdleTimeMillis(120000);        //minEvictableIdleTimeMiilis : Evict 쓰레드를 수행시, 만료여부를 체크할 시간을 정의                                             
+		ds.setNumTestsPerEvictionRun(20);                //numTestsPerEvictionRun : Evict 쓰레드를 수행시 수행할 커넥션의 갯수                                                       
+		ds.setRemoveAbandonedTimeout(30);                //removeAbandonedTimeout : 유효하지 안흔 커넥션의 삭제시의 타임아웃                                                           
+		ds.setRemoveAbandoned(true);                     //removeAbandoned : 유효하지 않는 커넥션의 제거 여부                                                                      
+		ds.setLogAbandoned(false);                       //logAbandoned : 유효하지 않는 커넥션을 생성한 코드 위치 로그생성 여부                                                             
 
 		
 		//ValidationQuery
 		// 일정시간마다 DB와 연결 확인
 		// mysql은 기본값으로 8시간동안
 		//DB에 요청이 없으면 DB 연결을 끊어 버린다
+//		ds.setMaxIdle(10);
+//		ds.setTestWhileIdle(true);
+//		ds.setValidationQuery("select 1");
 
-		ds.setTestWhileIdle(true);
-		ds.setValidationQuery("select 1");
-		ds.setValidationInterval(60000*60*4);
-		ds.setValidationQueryTimeout(10000);
-		ds.setMinEvictableIdleTimeMillis(60000*3);
-		ds.setTimeBetweenEvictionRunsMillis(10*1000);
+//		ds.setValidationQueryTimeout(10000);
+//		ds.setMinEvictableIdleTimeMillis(60000*3);
+		
+		
 		
 		
 		return ds;
